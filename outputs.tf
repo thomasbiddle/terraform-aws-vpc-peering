1ab58b17e17861797e4e13cec28b0df957a44f44
Prasanna <lakshmiprasanna@everest.engineering>
2019-07-24 12:26:33 +0530

    Upgrade to terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "peering_connection_id" {
-  value = "${aws_vpc_peering_connection.default.id}"
+  value = aws_vpc_peering_connection.default.id
 }
+
--- providers.tf
+++ providers.tf
@@ -5,3 +5,4 @@ provider "aws" {
 provider "aws" {
   alias = "dst"
 }
+
